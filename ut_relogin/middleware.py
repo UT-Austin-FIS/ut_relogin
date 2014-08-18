f07579687ed2e72fe91c19310124b7bfb6d22a9c
Eric Petersen <eric@huskers.unl.edu>
2014-08-18 18:11:40 -0500

    Inserting the scripts at the beginning of the head tag

--- ut_relogin/middleware.py
+++ ut_relogin/middleware.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 
 TAGS = """
-<script src="{static_url}ut_relogin/jquery-1.11.1.min.js"></script>
-<script src="{static_url}ut_relogin/jquery.utRelogin.js"></script>
-<script>
-    var $jqUtRelogin = jQuery.noConflict(true);
-    $jqUtRelogin.utRelogin();
-</script>
+<head>
+    <script src="{static_url}ut_relogin/jquery-1.11.1.min.js"></script>
+    <script src="{static_url}ut_relogin/jquery.utRelogin.js"></script>
+    <script>
+        var $jqUtRelogin = jQuery.noConflict(true);
+        $jqUtRelogin.utRelogin();
+    </script>
 """
 
 
@@ -15,6 +16,5 @@ class UtReloginHeadTagMiddleware(object):
     def process_response(self, request, response):
         if 'html' in response['Content-Type'].lower():
             tags = TAGS.format(static_url=settings.STATIC_URL)
-            tags = '\n'.join([tags, '</head>'])
-            response.content = response.content.replace(u'</head>', tags, 1)
+            response.content = response.content.replace(u'<head>', tags, 1)
         return response
