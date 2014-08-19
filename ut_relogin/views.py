7d991663b6cace526b3511833ec9337056e40669
Eric Petersen <eric@huskers.unl.edu>
2014-08-19 11:44:09 -0500

    fixing reference to template

--- ut_relogin/views.py
+++ ut_relogin/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render_to_response
 
 
 def redirected(request):
-    return render_to_response('redirected.html')
+    return render_to_response('ut_relogin/redirected.html')
