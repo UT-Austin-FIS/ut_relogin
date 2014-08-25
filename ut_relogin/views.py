15a4ce493b822f360f31b383ed8c790e1dcccfba
Todd Graves <tgraves@austin.utexas.edu>
2014-08-25 10:48:49 -0500

    adding customization to redirect template

--- ut_relogin/views.py
+++ ut_relogin/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render_to_response
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.views.generic.base import TemplateView
 
+try:
+    module_path, ctx_class = settings.RELOGIN_CONTEXT.rsplit('.',1)
+    module = __import__(module_path, fromlist=[ctx_class])
+    context = getattr(module, ctx_class)
+except AttributeError:
+    raise ImproperlyConfigured(
+        'You must supply a path your own context object by setting a '
+        'RELOGIN_CONTEXT in your settings.py file.'
+    )
 
-def redirected(request):
-    return render_to_response('ut_relogin/redirected.html')
+class ReloginRedirect(TemplateView):
+    template_name = 'ut_relogin/redirected.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(ReloginRedirect, self).get_context_data(**kwargs)
+        try:
+            msg = settings.RELOGIN_MESSAGE
+        except AttributeError:
+            raise ImproperlyConfigured(
+                'You must add a RELOGIN_MESSAGE to your settings. Example: '
+                'We have refreshed your login.  Thank you.'
+            )
+        ctx.update({'msg':msg})
+        return context(
+            self.request,
+            ctx,
+            title='UT Relogin Success',
+            page_title='UT Relogin Success',
+            window_title='UT Relogin Success',
+            api_key='8B54A49X54',
+            )
