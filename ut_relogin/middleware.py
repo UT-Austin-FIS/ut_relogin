3adfc8faa8fad921b701ce423ff087a2b664e358
Eric Petersen <eric@huskers.unl.edu>
2014-09-04 10:58:34 -0500

    Making middleware even more module by setting the final value as an attribute on the middleware class, so that we can wait to call reverse() until absolutely necessary.

--- ut_relogin/middleware.py
+++ ut_relogin/middleware.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-TAGS = """
+TAGS_TEMPLATE = """
 <head>
     <script src="{static_url}ut_relogin/jquery-1.11.1.min.js"></script>
     <script src="{static_url}ut_relogin/jquery.utRelogin.js"></script>
@@ -12,24 +12,26 @@ TAGS = """
             'formProtectUrl': '{form_url}',
         }});
     </script>
-""".format(
-    static_url=settings.STATIC_URL,
-    popup_url=reverse('ut_relogin_redirect'),
-    form_url=reverse('ut_relogin_form_protection'),
-)
+"""
 
 
 class UtReloginHeadTagMiddleware(object):
+    tags = None
 
     def get_head_tag_replacement(self):
         """
         Get the string that replaces <head> tags in HTML responses. Can be
-        overridden if TAGS is not quite what you're looking for.
+        overridden if TAGS_TEMPLATE is not quite what you're looking for.
         """
-        return TAGS
+        return TAGS_TEMPLATE.format(
+            static_url=settings.STATIC_URL,
+            popup_url=reverse('ut_relogin_redirect'),
+            form_url=reverse('ut_relogin_form_protection'),
+        )
 
     def process_response(self, request, response):
         if 'html' in response['Content-Type'].lower():
-            tags = self.get_head_tag_replacement()
-            response.content = response.content.replace(u'<head>', tags, 1)
+            if self.tags is None:
+                self.tags = self.get_head_tag_replacement()
+            response.content = response.content.replace(u'<head>', self.tags, 1)
         return response
